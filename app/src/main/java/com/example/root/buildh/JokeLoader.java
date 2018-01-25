450c593162dc6caad1fb3536c1e121c2609c8edf
root <hmostafa3bd@gmail.com>
2018-01-25 22:29:07 +0200

    Get URL from https://console.cloud.google.com/home/dashboard?project=composed-mason-192920&pli=1

--- app/src/main/java/com/example/root/buildh/JokeLoader.java
+++ app/src/main/java/com/example/root/buildh/JokeLoader.java
@@ -34,7 +34,7 @@ public class JokeLoader extends AsyncTask <Void,Void,String>{
 
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(),null)
-                    .setRootUrl("http://192.168.1.6:8080/_ah/api/")
+                    .setRootUrl("http://composed-mason-192920.appspot.com/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
